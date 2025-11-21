750e28fcd2c3a2b6d7d1305d88176594c7d4e011
L1EGE-KR1SH <krish.goel.w17@gmail.com>
2025-11-21 10:15:47 +0530

    completed 70%

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/Navbar";
-
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
