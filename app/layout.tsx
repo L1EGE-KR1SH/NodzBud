72f6803690f6f6e01083697f18c32765852af7c6
L1EGE-KR1SH <krish.goel.w17@gmail.com>
2025-11-22 21:47:28 +0530

    made scheduled area

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/Navbar";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
+import "react-datepicker/dist/react-datepicker.css"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
